4b46e458b46e5f9830f6a20f96c83ec03791a1f5
Cole <cj.colej@gmail.com>
2019-06-18 22:26:59 -0700

    initial commit

---
+++ resources/views/profiles/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+        <img class="rounded-circle w-100" src="/storage/{{ $user->profile->image}}">
+        </div>
+        <div class="col-9 pt-5">
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username }}</h1>
+                @can('update', $user->profile)
+                    <a href="/p/create">Add New Post</a>
+                @endcan
+            </div>
+            @can('update', $user->profile)
+                <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
+            @endcan
+            <div class="d-flex">
+                <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> Posts</div>
+                <div class="pr-5"><strong>23k</strong> Followers</div>
+                <div class="pr-5"><strong>212</strong> Following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href="#">{{ $user->profile->url }}</a></div>
+        </div>
+    </div>
+    
+    <div class="row">
+        @foreach($user->posts as $post)
+            <div class="col-4 pb-4">
+                <a href="/p/{{ $post->id }}">
+                    <img class="w-100 pt-5" src="/storage/{{ $post->image }}" alt="">
+                </a>
+            </div>
+        @endforeach
+    </div>
+</div>
+@endsection
